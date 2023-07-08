f592c5e9b1cf96e1279e14840fce22e38e158543
Carmen Llorente <carmenllorenteb@gmail.com>
2023-07-08 10:50:14 +0200

    Obtenemos el valor de N

---
+++ matriz.py
@@ -0,0 +1,9 @@
+#!/usr/bim/env python3
+
+import random
+
+N = input("Introduzca el tamaño de la matriz: ")
+
+print(f"La matriz a crear será de {N}x{N}.")
+
+print(f"Un valor aleatorio entre 0 y 9: {random.randint(0,9)}")
