b421f1ec36dfe185215e0a7f0392a6b316b64c47
Carmen Llorente <carmenllorenteb@gmail.com>
2023-07-08 11:05:16 +0200

    +control de errores

--- matriz.py
+++ matriz.py
@@ -2,8 +2,20 @@
 
 import random
 
-N = input("Introduzca el tamaño de la matriz: ")
+# Add control de errores (excepciones si no se introduce el valor esperado)
+while True:
+    try:
+        cadena = input("Introduce un número: ")
+        N = int(cadena)
+        break  # Salimos del bucle si la conversión es exitosa
+    except ValueError:
+        print("Esperaba un número entero. Inténtalo de nuevo.")
 
+
+# tamaño de la matriz (N):
 print(f"La matriz a crear será de {N}x{N}.")
+
+
+
 
 print(f"Un valor aleatorio entre 0 y 9: {random.randint(0,9)}")
